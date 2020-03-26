4db93cd378d5f247d171726539573391323929db
dalexandrina <dina.bizgu.99@gmail.com>
2020-03-26 22:11:22 +0200

    first commit

---
+++ Assets/InitScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitScript : MonoBehaviour {
+    public GameObject cell;
+    GameObject [ , ] cells;
+
+    void Start() {
+        Instantiate(cell, Vector3.zero, Quaternion.identity);
+    }
+
+    void Update() {
+        if(Input.GetMouseButton(0)) {
+            Vector3 clicked = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            clicked.z = 0;
+            Instantiate(cell, clicked, Quaternion.identity);
+        }
+    }
+}
